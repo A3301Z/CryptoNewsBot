a03edd4adee3d460a593bf09b85750589761c03d
Artur <artur.zubkov111@gmail.com>
2023-08-12 15:16:16 +0300

    Modyfied pom.xml: jdbc, mysql driver, junit, commons-codec

---
+++ src/main/java/org/example/Registration.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Registration {
+
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		Registration registration = new Registration();
+		registration.connectDB();
+	}
+
+	public void connectDB() throws SQLException, ClassNotFoundException {
+		String url = "jdbc:mysql://localhost:3306/cryptonewsbot";
+		String name = "root";
+		String pass = "Ghbdtn564896";
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		try (Connection connection = DriverManager.getConnection(url, name, pass)) {
+			System.out.println("Connecting, people!");
+		}
+	}
+}
